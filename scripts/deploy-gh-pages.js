a8b74ee55ce4f1883ee12869511265156746dcc1
Arnab Karmakar <arnab.k@gmail.com>
2019-03-11 00:49:11 -0600

    Fix git checkout command

--- scripts/deploy-gh-pages.js
+++ scripts/deploy-gh-pages.js
@@ -6,7 +6,7 @@ const gitPassword = process.env.GITHUB_TOKEN;
 
 (async () => {
   // Checkout code
-  await exec(`git checkout -b gh-pages https://${gitUsername}:${gitPassword}@github.com/arnabk/resume`);
+  await exec(`git clone -b gh-pages https://${gitUsername}:${gitPassword}@github.com/arnabk/resume`);
   await exec("cp -R ./build/* resume/");
   await exec('git add --all');
   await exec('git commit -m "Autocommit by Jenkins - Updating gh-pages"');
